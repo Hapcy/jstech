341dff3348efac003f3fd07e1e885038a68b3cac
Móger Tibor (SmartX) <tibor.moger@smartx.eu>
2019-09-18 15:13:18 +0200

    add xhr helper func

--- 1/index.js
+++ 1/index.js
@@ -1,8 +1,28 @@
 const kereses = document.querySelector('#kereses');
-kereses.type = "button";
+kereses.type = 'button';
 const sorozatCim = document.querySelector('#sorozat-cim');
 kereses.addEventListener('click', () => {
-  fetch(`http://api.tvmaze.com/search/shows?q=${sorozatCim.value}`)
-    .then(resp => resp.json())
-    .then(sorozatok => console.log(sorozatok));
+
 });
+
+function xhr(method, url, data, cb) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  xhr.addEventListener('load', () => {
+    let { response, status } = xhr;
+    let res = JSON.parse(response);
+    if (status >= 200 && status < 400) {
+      cb({ response: res, status });
+    } else {
+      cb({ response: res, status });
+    }
+  });
+
+  if (data) {
+    xhr.send(JSON.stringify(data));
+  } else {
+    xhr.send();
+  }
+}
